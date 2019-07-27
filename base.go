778bbadd543539018c4817591d2f0324cca1374c
Joshua Thornton <joshua_thornton1@homedepot.com>
2019-07-27 10:16:08 -0700

    updates

--- base.go
+++ base.go
@@ -1,30 +1,36 @@
 package di_test
 
 import (
-	"fmt"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 type Zerolog struct {
+	// TimeFieldFormat sets the zerolog.TimeFieldFormat
 	TimeFieldFormat string
+	// GlobalLevel is passed to zerolog.SetGlobalLevel
+	GlobalLevel int
 }
-
 
 type Config struct {
 	Zerolog Zerolog
-	Log Log
+	Log     Log
 }
 
 // Configure sets up objects with configuration given in Config.
 func (c *Config) Configure() {
-	if c.Zerolog.TimeFieldFormat == "" {
-		fmt.Println("setting time field format default")
-		zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	// set zerolog defaults
+	{
+		if c.Zerolog.TimeFieldFormat == "" {
+			zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+		}
+		if c.Zerolog.GlobalLevel == 0 {
+			zerolog.SetGlobalLevel(zerolog.WarnLevel)
+		}
 	}
 }
 
-type Log struct {}
+type Log struct{}
 
 // Printf is a mirror of zerolog's log.Printf.
 func (c *Log) Printf(format string, v ...interface{}) {
