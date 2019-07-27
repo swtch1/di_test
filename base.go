783ac784a834526a262726f08c2ff3f04af9d183
Joshua Thornton <joshua_thornton1@homedepot.com>
2019-07-27 10:38:23 -0700

    updates

--- base.go
+++ base.go
@@ -15,7 +15,7 @@ type Zerolog struct {
 
 type Config struct {
 	Zerolog Zerolog
-	Log     zerolog.Event
+	Log     Log
 }
 
 // Configure sets up objects with configuration given in Config.
@@ -35,11 +35,32 @@ func (c *Config) Configure() *Config {
 
 type Log struct{}
 
-//// Printf is a mirror of zerolog's log.Printf.
-//func (_ *Log) Printf(format string, v ...interface{}) {
-//	log.Printf(format, v)
-//}
-//
-//func (_ *Log) Warn() *zerolog.Event {
-//	return log.Warn()
-//}
+// Printf is a mirror of zerolog's log.Printf().
+func (_ *Log) Printf(format string, v ...interface{}) {
+	log.Printf(format, v)
+}
+
+// Debug is a mirror of zerolog's log.Debug().
+func (_ *Log) Debug() *zerolog.Event {
+	return log.Debug()
+}
+
+// Info is a mirror of zerolog's log.Info().
+func (_ *Log) Info() *zerolog.Event {
+	return log.Info()
+}
+
+// Warn is a mirror of zerolog's log.Warn().
+func (_ *Log) Warn() *zerolog.Event {
+	return log.Warn()
+}
+
+// Error is a mirror of zerolog's log.Error().
+func (_ *Log) Error() *zerolog.Event {
+	return log.Error()
+}
+
+// Fatal is a mirror of zerolog's log.Fatal().
+func (_ *Log) Fatal() *zerolog.Event {
+	return log.Fatal()
+}
